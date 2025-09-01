f6a8bbf6028abe6a9d5118fe3de0229ec62f98ae
duwlsdl111 <duwlsdl75@gmail.com>
2025-09-01 16:33:04 +0900

    박여진 팀장 첫번째 커밋
    망치는 자 혼날지어다

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Install from './install';
+import StartRating from './components/StarRating';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PWA 테스트</h1>
+      <Install />
+      <StartRating itemId={1} />
+      <StartRating itemId={2} />
+      <StartRating itemId={3} />
+    </div>
+  );
+}
+
+export default App;
